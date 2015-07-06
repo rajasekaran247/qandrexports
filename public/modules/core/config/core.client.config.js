e92375de62a4b3c5bfa4e724dbae57f6b24e6541
rajasekaran247 <rajasekaran@247lifetime.com>
2015-07-06 16:00:38 +0530

    initial commit

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -6,5 +6,11 @@ angular.module('core').run(['Menus',
     Menus.addMenuItem('topbar', 'Setup', 'setup', 'dropdown');
     Menus.addSubMenuItem('topbar', 'setup', 'Client', 'clients'); 
     Menus.addSubMenuItem('topbar', 'setup', 'Settings', 'settings'); 
+    Menus.addSubMenuItem('topbar', 'setup', 'Products', 'products'); 
+    Menus.addMenuItem('topbar', 'Transactions', 'transactions', 'dropdown');
+    Menus.addSubMenuItem('topbar', 'transactions', 'Orders', 'orders'); 
+    Menus.addSubMenuItem('topbar', 'transactions', 'Projects', 'projects'); 
+    Menus.addSubMenuItem('topbar', 'transactions', 'Invoices', 'invoices');
+    Menus.addSubMenuItem('topbar', 'transactions', 'Expenses', 'expenses');
   }
 ]);
